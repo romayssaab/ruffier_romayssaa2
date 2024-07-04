316881f0058c359a3bdde4423f2464a147f45fb4
Romayssaa <programmationromayssaa@gmail.com>
2024-07-04 19:42:56 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,3 +1,4 @@
+print("Changes!")
 from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
 from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont # checking the types of input values
 from PyQt5.QtWidgets import (
